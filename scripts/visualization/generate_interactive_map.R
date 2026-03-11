aed6b4994fc147872b67eb8d6370f539ffb73b00
anihot <anika.hotzel@hs-bochum.de>
2026-03-11 08:54:32 +0100

    Enhance interactive map popups with latest sensor data and fix empty popup issue

--- scripts/visualization/generate_interactive_map.R
+++ scripts/visualization/generate_interactive_map.R
@@ -22,17 +22,58 @@ if (!dir_exists(path_dir(output_file))) {
 # 1. Load coordinates
 sensors <- read_csv(metadata_file, show_col_types = FALSE)
 
-# 2. Create Leaflet map
+# 2. Add latest data to popups
+cat("Fetching latest data for popups...\n")
+cleaned_dir <- "data/processed/cleaned_analysis"
+
+sensors <- sensors %>%
+    rowwise() %>%
+    mutate(
+        # Attempt to find the cleaned data file for this station
+        safe_name = gsub("[^[:alnum:]]", "_", station),
+        file_glob = paste0(cleaned_dir, "/", safe_name, "*_cleaned.csv"),
+        files = list(Sys.glob(file_glob)),
+        latest_level = NA_real_,
+        latest_time = NA_character_,
+        
+        # If file exists, read the last line
+        has_data = length(files) > 0,
+        temp_data = if(has_data) {
+            tryCatch({
+                d <- read_csv(files[[1]], show_col_types = FALSE, n_max = 50, skip = max(0, count_fields(files[[1]], tokenizer_csv()) - 50))
+                tail(d, 1)
+            }, error = function(e) NULL)
+        } else { NULL }
+    ) %>%
+    mutate(
+        latest_level = if(!is.null(temp_data)) temp_data$level else NA_real_,
+        latest_time = if(!is.null(temp_data)) format(temp_data$Zeit_Datum, "%Y-%m-%d %H:%M") else "No recent data"
+    ) %>%
+    ungroup()
+
+# Construct Popup Content
+sensors <- sensors %>%
+    mutate(
+        popup_content = paste0(
+            "<div style='font-family: Arial, sans-serif;'>",
+            "<b>Station: </b>", station, "<br>",
+            "<b>Label: </b>", label, "<br><hr>",
+            "<b>Aktueller Stand: </b>", ifelse(is.na(latest_level), "N/A", paste0(latest_level, " m")), "<br>",
+            "<b>Zeitpunkt: </b>", latest_time,
+            "</div>"
+        )
+    )
+
+# 3. Create Leaflet map
 m <- leaflet(sensors) %>%
-    addTiles() %>% # Add default OpenStreetMap map tiles
-    addProviderTiles(providers$CartoDB.Positron) %>% # Add a clean, light base map
+    addTiles() %>% 
+    addProviderTiles(providers$CartoDB.Positron) %>% 
     addMarkers(
         lng = ~lon,
         lat = ~lat,
-        popup = ~ paste0("<b>Station: ", station, "</b><br>", label),
+        popup = ~popup_content,
         label = ~station
     ) %>%
-    # Add a mini map in the corner
     addMiniMap(toggleDisplay = TRUE)
 
 # 3. Save as HTML
